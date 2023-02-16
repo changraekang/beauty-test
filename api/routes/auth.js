98cf17279eb0a76ead54f42263e3116826055cf3
kangchangrae <storkcr@gmail.com>
2023-02-16 13:30:16 +0900

    change name

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,15 +1,10 @@
 const express = require("express");
-const {
-  login,
-  logout,
-  register,
-  getRanking,
-} = require("../controllers/auth.js");
+const { login, logout, register, getUser } = require("../controllers/auth.js");
 
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
-router.get("/getRanking", getRanking);
+router.get("/getUser", getUser);
 
 module.exports = router;
